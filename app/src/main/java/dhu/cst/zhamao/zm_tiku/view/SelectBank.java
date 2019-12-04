f18fea2f5126f7d48e4f06539328622b9f5265d3
Hugo Swift <codefinchy@gmail.com>
2019-12-04 20:38:45 +0800

    调整UI

--- app/src/main/java/dhu/cst/zhamao/zm_tiku/view/SelectBank.java
+++ app/src/main/java/dhu/cst/zhamao/zm_tiku/view/SelectBank.java
@@ -6,8 +6,16 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import dhu.cst.zhamao.zm_tiku.R;
 
@@ -51,6 +59,31 @@ public class SelectBank extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SelectBank.this, SelectMode.class);
                 startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(SelectBank.this).toBundle());
+            }
+        });
+
+        final FloatingActionButton updateButton = findViewById(R.id.upateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation circle_anim = AnimationUtils.loadAnimation(SelectBank.this, R.anim.rotate);
+                LinearInterpolator interpolator = new LinearInterpolator();  //设置匀速旋转，在xml文件中设置会出现卡顿
+                circle_anim.setInterpolator(interpolator);
+                updateButton.startAnimation(circle_anim);  //开始动画
+                Timer updateResourceTimer = new Timer();
+                TimerTask mTimerTask = new TimerTask() {//创建一个线程来执行run方法中的代码
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable(){
+                            @Override
+                            public void run(){
+                                updateButton.clearAnimation();
+                                Snackbar.make(findViewById(R.id.ConstraintLayout),"成功更新题库",Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                };
+                updateResourceTimer.schedule(mTimerTask, 3000);
             }
         });
     }
